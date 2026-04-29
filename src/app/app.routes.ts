c3281b9049a970669f2dd7b7a7b852d46fc81b0d
Anna Vasilevich <dzichonka@gmail.com>
2026-04-29 15:13:28 +0200

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./pages/home-page/home-page.component').then((m) => m.HomePageComponent),
+      },
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        //   path: ':movieId',
+        path: 'details',
+        loadComponent: () =>
+          import('./pages/details-page/details-page.component').then((m) => m.DetailsPageComponent),
+      },
+      {
+        path: '**',
+        loadComponent: () =>
+          import('./pages/not-found-page/not-found-page.component').then(
+            (m) => m.NotFoundPageComponent,
+          ),
+      },
+    ],
+  },
+];
